8b5546a7dae9af82b01cb3916ba5940ccd7f16e4
Anija Anilkumar <v-aanilkumar@expediagroup.com>
2022-09-29 09:27:29 +0530

    Initial Commit

---
+++ src/main/java/com/hotelpad/hotelpad/HotelPadApplication.java
@@ -0,0 +1,15 @@
+package com.hotelpad.hotelpad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class HotelPadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelPadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotelpad/hotelpad/HotelPadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelpad.hotelpad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelPadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

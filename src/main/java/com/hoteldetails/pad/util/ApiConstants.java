2bf3a80cda8f72ffb8ed19a9b5b8614fb18f5b3c
ArjunAruz <a-10375@ibsplc.com>
2022-11-10 14:47:14 +0530

    [TECH-508] Code review changes

--- src/main/java/com/hoteldetails/pad/util/ApiConstants.java
+++ src/main/java/com/hoteldetails/pad/util/ApiConstants.java
@@ -7,5 +7,6 @@ public final class ApiConstants {
     public static final String SEPERATOR = "";
     public static final int SUCCESS = 1;
     public static final int FAILURE = 0;
+    public static final String TYPE = "1";
 
 }
